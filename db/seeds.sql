b1335971279da0fad125cf7c62e03884723f9bfe
Wilhelm Picaco <wpicaco@gmail.com>
2018-10-31 20:39:28 -0600

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name) VALUES ("Big Burger");
+INSERT INTO burgers (burger_name) VALUES ("Bigger Burger");
+INSERT INTO burgers (burger_name) VALUES ("Giganto Burger");
+INSERT INTO burgers (burger_name) VALUES ("Monstrosity Burger");
+INSERT INTO burgers (burger_name) VALUES ("Obscene Burger");
+INSERT INTO burgers (burger_name) VALUES ("Wheelbarrow Burger");
+INSERT INTO burgers (burger_name) VALUES ("Patty Cake Burger");
+INSERT INTO burgers (burger_name) VALUES ("Button Popper Burger");
